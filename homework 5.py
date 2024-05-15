3b88f56e26f97dee8468a2e25ff085fc0d6474b5
Alxdr02 <nagorntip163@gmail.com>
2024-05-15 13:40:11 +0400

    Add files via upload

---
+++ homework 5.py
@@ -0,0 +1,15 @@
+my_list=['яблоко', 'банан', 'киви','груша','клубника','арбуз']
+print(my_list)
+print(my_list[:1],my_list[-1:])
+print(my_list[2:5])
+my_list[2]='кокос'
+print(my_list)
+my_dict={'apple': 'яблоко','banan':'банан','orange':'апельсин'}
+print(my_dict)
+print(my_dict.keys())
+my_dict.update({'pineapple':'ананас'})
+print(my_dict)
+my_dict['Tomato']= 'помидор'
+print(my_dict)
+my_dict['Tomato']= 'дыня'
+print(my_dict)
