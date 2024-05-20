64edcb5b87bce877dbc61c8451a5aeccdbbf239a
Alxdr02 <nagorntip163@gmail.com>
2024-05-21 00:42:02 +0400

    Add files via upload

---
+++ homework for.py
@@ -0,0 +1,8 @@
+cars_ = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+for i in cars_:
+    for j in ['Я езжу на автомобиле марки ']:
+        print(f'{j + i}')
+
+cars_ = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count = 0
+
